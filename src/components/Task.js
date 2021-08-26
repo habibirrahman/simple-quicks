c3c1cfe2eb44864fec1970c0cd74d6dcccf89e33
habibirrahman <habibirrahman18@gmail.com>
2021-08-26 22:26:38 +0700

    add task item structure

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
-import { Dropdown, Button } from "semantic-ui-react";
+import { Dropdown, Button, Checkbox } from "semantic-ui-react";
 
 import InboxIcon from "../assets/inbox.svg";
 import TaskOpen from "../assets/task-open.svg";
 import BackIcon from "../assets/back.svg";
+
+import ExpandUp from "../assets/expand-up.svg";
+// import ExpandDown from "../assets/expand-down.svg";
+import ThreePoint from "../assets/three-point.svg";
+import Clock from "../assets/clock.svg";
+import Calendar from "../assets/calendar.svg";
+import Pen from "../assets/pen.svg";
 
 const getOptions = () => [
     {
@@ -25,7 +32,7 @@ const getOptions = () => [
 ];
 
 const Task = (props) => {
-    const [isTaskShow, setTaskShow] = useState(true);
+    const [isTaskContentShow, setTaskContentShow] = useState(false);
     const [dropdownValue, setDropdownValue] = useState(getOptions()[0].text);
     // const [isContentShow, setContentShow] = useState(false);
     const _handleChangeDropdown = (e, { value }) => {
@@ -33,12 +40,18 @@ const Task = (props) => {
         console.log(value);
     };
     const _handleNewTask = () => {
-        console.log('new task')
-    }
+        console.log("new task");
+    };
+
+    useEffect(() => {
+        let timer = setTimeout(() => {
+            setTaskContentShow(true);
+        }, 300);
+        return () => clearTimeout(timer);
+    });
     return (
         <React.Fragment>
             {/* box of task */}
-            {isTaskShow && null}
             <div className={props.taskCard}>
                 <div className="task-header">
                     <Dropdown
@@ -46,7 +59,6 @@ const Task = (props) => {
                         options={getOptions()}
                         value={dropdownValue}
                         onChange={_handleChangeDropdown}
-                        ref={null}
                     />
                     <Button
                         primary
@@ -63,6 +75,71 @@ const Task = (props) => {
                     Task Management Service <br />
                     Coming Soon ...
                 </Loader> */}
+                {isTaskContentShow && (
+                    <div className="task-content">
+                        <div className="item">
+                            <div className="info">
+                                <div className="left">
+                                    <Checkbox
+                                        style={{ marginTop: "1px" }}
+                                        // onChange={}
+                                        // onClick={}
+                                    />
+                                    <div className="title">
+                                        Set up documentation report for several
+                                        Cases : Case 145443, Case 192829 and
+                                        Case 182203
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <div className="limit">4 Days Left</div>
+                                    <div className="date">14/06/2021</div>
+                                    <img
+                                        className="details"
+                                        src={ExpandUp}
+                                        alt="ExpandUp"
+                                    />
+                                    <img
+                                        className="menu"
+                                        src={ThreePoint}
+                                        alt="Menu"
+                                    />
+                                </div>
+                            </div>
+                            <div className="action">
+                                <div className="date-info">
+                                    <img
+                                        className="clock"
+                                        src={Clock}
+                                        alt="Clock"
+                                    />
+                                    <div className="set-date">
+                                        <div className="date-value">
+                                            14/06/2021
+                                        </div>
+                                        <img
+                                            className="calendar-icon"
+                                            src={Calendar}
+                                            alt="Calendar"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="description">
+                                    <img className="pen" src={Pen} alt="Pen" />
+                                    <div className="set-description">
+                                        Closing off this case since this
+                                        application has been cancelled. No one
+                                        really understand how this case could
+                                        possibly be cancelled. The options and
+                                        the documents within this document were
+                                        totally a guaranteed for a success!
+                                    </div>
+                                </div>
+                            </div>
+                            <hr className="devider" />
+                        </div>
+                    </div>
+                )}
             </div>
 
             {/* other submenu */}
